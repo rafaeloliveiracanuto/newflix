4077b0c53b2f17a71f9d245f2d448aa071919869
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-09-09 09:22:53 -0300

    Add properties and methods

--- app/src/main/java/com/example/newflix/model/Movie.java
+++ app/src/main/java/com/example/newflix/model/Movie.java
@@ -1,4 +1,15 @@
 package com.example.newflix.model;
 
 public class Movie {
+    private int coverUrl;
+
+    public int getCoverUrl() {
+        return coverUrl;
+    }
+
+    public void setCoverUrl(int coverUrl) {
+        this.coverUrl = coverUrl;
+    }
+
+
 }
